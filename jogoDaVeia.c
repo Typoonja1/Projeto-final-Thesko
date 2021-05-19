02ec94bc8abbc9292e69ea354e0a5ec89b6d31dd
rafael556 <rafael7jf@hotmail.com>
2021-05-19 20:11:09 -0300

    v1

--- jogoDaVeia.c
+++ jogoDaVeia.c
@@ -4,18 +4,33 @@ void printMatriz(char matriz[][4]){
 
     for (int i = 0; i < 4; i++){
         for (int j = 0; j < 4; j++){
-
-
-            
             printf("%c ", matriz[i][j]);
         }
         printf("\n");
     }
 }
 
+void verificacao(int jogador){
+
+}
+
+void verificarGanhador(int ganhador, char matriz[][4]){
+
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            
+        }
+        
+    }
+}
+
+void jogar(nlinha, ncoluna, jogador){
+
+}
+
 int main(){
 
-    int jogo = 0, ganhador, veia = 1;
+    int ganhador = 0, jogo = 1;
     char joguinho[4][4];
 
     for (int i = 0; i < 4; i++){
@@ -27,24 +42,29 @@ int main(){
             else if(j == 0){
                 joguinho[i][j] = i + 1;
             }
-            else if(i == 0 && j == 0){
-                joguinho[i][j] = ' ';
-                // continue;
-            }
             else
             joguinho[i][j] = '-';
+            
+            if(i == 0 && j == 0)
+                joguinho[i][j] = ' ';
         }  
     }
     
     printMatriz(joguinho);
     
 
-    // while(veia){
+    while(jogo){
 
-    //     while(jogo != 1){
+        while(ganhador != 1){
 
-    //     }
-    // }
+            verificarGanhador(ganhador);
+
+            verificacao(1);
+
+            verificacao(2);
+
+        }
+    }
 
     return 0;
 }
