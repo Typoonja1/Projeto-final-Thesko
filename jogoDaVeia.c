639254f781879ecd4b8a6d2ec757723ed3d1ca3b
rafael556 <rafael7jf@hotmail.com>
2021-05-20 18:10:18 -0300

    v2

--- jogoDaVeia.c
+++ jogoDaVeia.c
@@ -10,22 +10,52 @@ void printMatriz(char matriz[][4]){
     }
 }
 
-void verificacao(int jogador){
+void verificacao(int jogador, char matriz[][4]){
 
+    int nlinha, ncoluna;
+
+        scanf("%d",&nlinha);
+        scanf("%d",&ncoluna);
+
+    while(matriz[nlinha][ncoluna] == '-'){ ///////////////////////////
+        
+        
+        if(jogador == 1)
+            jogar(nlinha, ncoluna, matriz, 'X');
+        
+        if(jogador == 2)
+            jogar(nlinha, ncoluna, matriz, 'O');
+
+        printMatriz(matriz);
+    }
 }
-
+/////////////////////////////////////////////////////////////////////////
 void verificarGanhador(int ganhador, char matriz[][4]){
 
+    int controle1 = 0;
+    int controle2 = 0;
+    int controle3 = 0;
     for (int i = 0; i < 4; i++){
         for (int j = 0; j < 4; j++){
-            
+         
+         if(i >= 1){
+            if(matriz[i][j] == 'X' || matriz[i][j] == 'O'){
+                controle1++;
+            }
+
+            if( i == j){
+                if(matriz[i][j] == 'X' || matriz[i][j] == 'O'){
+                    controle2++;
+                }
+            }
+         }   
         }
         
     }
 }
-
-void jogar(int nlinha, int ncoluna, char simbolo){
-
+/////////////////////////////////////////////////////////////////////////
+void jogar(int nlinha, int ncoluna, char matriz[][4], char simbolo){
+    matriz[nlinha][ncoluna] = simbolo;
 }
 
 int main(){
@@ -57,11 +87,11 @@ int main(){
 
         while(ganhador != 1){
 
-            verificarGanhador(ganhador);
+            verificarGanhador(ganhador, joguinho);
 
-            verificacao(1);
+            verificacao(1, joguinho);
 
-            verificacao(2);
+            verificacao(2, joguinho);
 
         }
     }
