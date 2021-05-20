528c954a5d5f9f619f12e226253171dc5a8daa2d
rafael556 <rafael7jf@hotmail.com>
2021-05-20 19:20:10 -0300

    v3

--- jogoDaVeia.c
+++ jogoDaVeia.c
@@ -10,53 +10,73 @@ void printMatriz(char matriz[][4]){
     }
 }
 
+void jogar(int nlinha, int ncoluna, char matriz[][4], char simbolo){
+    matriz[nlinha][ncoluna] = simbolo;
+}
+
 void verificacao(int jogador, char matriz[][4]){
 
-    int nlinha, ncoluna;
+    int nlinha, ncoluna, loop = 1;;
 
-        scanf("%d",&nlinha);
-        scanf("%d",&ncoluna);
 
-    while(matriz[nlinha][ncoluna] == '-'){ ///////////////////////////
+    while(loop){ ///////////////////////////
+        printf("Digite a linha e coluna respectivamente!\n");
+        scanf("%d %d",&nlinha, &ncoluna);
         
-        
-        if(jogador == 1)
-            jogar(nlinha, ncoluna, matriz, 'X');
-        
-        if(jogador == 2)
-            jogar(nlinha, ncoluna, matriz, 'O');
+        if(matriz[nlinha][ncoluna] == '-'){
+            if(jogador == 1)
+                jogar(nlinha, ncoluna, matriz, 'X');
+            
+            if(jogador == 2)
+                jogar(nlinha, ncoluna, matriz, 'O');
 
-        printMatriz(matriz);
+            printMatriz(matriz);
+            loop = 0;
+        }
+        else{
+            printf("Posicao invalida!\n\n");
+        }
     }
 }
 /////////////////////////////////////////////////////////////////////////
 void verificarGanhador(int ganhador, char matriz[][4]){
 
-    int controle1 = 0;
-    int controle2 = 0;
-    int controle3 = 0;
-    for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++){
+    int linha1 = 0, linha2 = 0, diagonalP1 = 0, diagonalP2 = 0, diagonalS1 = 0, diagonalS2 = 0;
+
+    for (int i = 1; i < 4; i++){
+        for (int j = 1; j < 4; j++){
          
-         if(i >= 1){
-            if(matriz[i][j] == 'X' || matriz[i][j] == 'O'){
-                controle1++;
+            if(matriz[i][j] == 'X'){
+                linha1++;
+            }
+
+            if(matriz[i][j] == '0'){
+                linha2++;
             }
 
             if( i == j){
-                if(matriz[i][j] == 'X' || matriz[i][j] == 'O'){
-                    controle2++;
+                if(matriz[i][j] == 'X'){
+                    diagonalP1++;
                 }
             }
-         }   
+
+            if( i == j){
+                if(matriz[i][j] == 'O'){
+                    diagonalP2++;
+                }
+            }   
         }
-        
+    }
+
+    if(linha1 == 3 || diagonalP1 == 3 || diagonalS1 == 3){
+        ganhador = 1;
+    }
+
+    if(linha2 == 3 || diagonalP2 == 3 || diagonalS2 == 3){
+        ganhador = 2;
     }
 }
 /////////////////////////////////////////////////////////////////////////
-void jogar(int nlinha, int ncoluna, char matriz[][4], char simbolo){
-    matriz[nlinha][ncoluna] = simbolo;
-}
 
 int main(){
 
@@ -85,14 +105,15 @@ int main(){
 
     while(jogo){
 
-        while(ganhador != 1){
-
-            verificarGanhador(ganhador, joguinho);
+        while(ganhador == 0){
 
             verificacao(1, joguinho);
 
+           verificarGanhador(ganhador, joguinho);
+
             verificacao(2, joguinho);
 
+           verificarGanhador(ganhador, joguinho);
         }
     }
 
